aaae4f62447a4f4a6c64bc1ebb37414ca4548078
AidenLu2110 <142829225+AidenLu2110@users.noreply.github.com>
2024-03-11 17:06:17 +0000

    Mon, Mar 11, 2024, 10:06 AM -07:00

--- js/landingPage.js
+++ js/landingPage.js
@@ -3,14 +3,24 @@ open = document.getElementById('open')
 modal = document.getElementById('moda')
 close = document.getElementById('close')
 
+// Toggle Nav
 toggle.addEventListener('click', () => {
     document.body.classlist.toggle('show-nav')
 })
 
+//Show Modal
 open.addEventListener('click', () => {
     modal.classList.add('show-modal')
 })
 
+//Hide Modal
 close.addEventListener('click', () => {
     modal.classList.remove('show-modal')
+})
+
+//Hide Modal on Outside click
+window.addEventListener('click', (e) => {
+    if (e.target == modal) {
+        modal.classList.remove('show-modal')
+    }
 })
