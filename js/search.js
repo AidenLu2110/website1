eea36e8c462722191e448b42d6b10b6dbda1f37b
AidenLu2110 <142829225+AidenLu2110@users.noreply.github.com>
2024-03-13 16:30:35 +0000

    Wed, Mar 13, 2024, 9:30 AM -07:00

--- js/search.js
+++ js/search.js
@@ -4,3 +4,9 @@ search = document.querySelector('.search')
 btn.addEventListener('click', () => {
     search.classList.toggle('active')
 })
+
+function searchInput() {
+    if (input = index) {
+        
+    }
+}
