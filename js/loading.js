cbdebb79fcf4607ab49fcb8e433fa07c1c222e58
AidenLu2110 <142829225+AidenLu2110@users.noreply.github.com>
2024-03-15 16:33:10 +0000

    Fri, Mar 15, 2024, 9:33 AM -07:00

--- js/loading.js
+++ js/loading.js
@@ -18,3 +18,23 @@ function blurring() {
     loadText.style.opacity = 1 - load/100
     bg.style.filter = `blur(${30 - (load/100)*30}px)`
 }
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
